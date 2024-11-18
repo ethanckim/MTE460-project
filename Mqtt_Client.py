1094e5ac888dedfb223e366560241f9849af7261
Ethan Kim <ethanc.kim@uwaterloo.ca>
2024-11-18 10:57:48 -0500

    update mqtt_client.py

--- Mqtt_Client.py
+++ Mqtt_Client.py
@@ -1,35 +1,36 @@
 import random
-from paho.mqtt import client as mqtt_client
+import paho.mqtt.client as mqtt
 
-class MqttClient:
-    def connect_mqtt(broker, port, client_id):
-        def on_connect(client, userdata, flags, rc):
-        # For paho-mqtt 2.0.0, you need to add the properties parameter.
-        # def on_connect(client, userdata, flags, rc, properties):
-            if rc == 0:
-                print("Connected to MQTT Broker!")
-            else:
-                print("Failed to connect, return code %d\n", rc)
-        # Set Connecting Client ID
-        client = mqtt_client.Client(client_id)
+def on_connect(mqttc, obj, flags, reason_code, properties):
+    print("reason_code: " + str(reason_code))
 
-        # For paho-mqtt 2.0.0, you need to set callback_api_version.
-        client = mqtt_client.Client(client_id=client_id, callback_api_version=mqtt_client.CallbackAPIVersion.VERSION2)
 
-        # client.username_pw_set(username, password)
-        client.on_connect = on_connect
-        client.connect(broker, port)
-        return client
+def on_message(mqttc, obj, msg):
+    print(msg.topic + " " + str(msg.qos) + " " + str(msg.payload))
 
-if __name__ == "__main__":
-    mqtt_client = MqttClient()
 
-    broker = 'mqtt.eclipse.org'
-    port = 1883
-    topic = "python/mqtt"
-    client_id = f'publish-{random.randint(0, 1000)}'
+def on_publish(mqttc, obj, mid, reason_code, properties):
+    print("mid: " + str(mid))
 
-    client = mqtt_client.connect_mqtt(broker, port, client_id)
-    client.loop_start()
-    client.publish(topic, 'test message')
-    client.loop_stop()
+
+def on_log(mqttc, obj, level, string):
+    print(string)
+
+
+mqttc = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+
+mqttc.on_message = on_message
+mqttc.on_connect = on_connect
+mqttc.on_publish = on_publish
+# Uncomment to enable debug messages
+# mqttc.on_log = on_log
+mqttc.connect(host="mqtt.eclipse.org", port=1883)
+
+mqttc.loop_start()
+
+print("tuple")
+(rc, mid) = mqttc.publish(topic="tuple", payload="bar", qos=2)
+print("class")
+infot = mqttc.publish(topic="class", payload="bar", qos=2)
+
+infot.wait_for_publish()
